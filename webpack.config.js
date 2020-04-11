7ee19859be7d92816cfde740f350bb31856bc50e
phucnv282 <phucnv282@gmail.com>
2020-04-11 22:56:19 +0700

    add/convert2HexColor-utils.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 module.exports = function(env) {
-    let entry = env.MAIN || './src/main/basic.js';
-    console.log(entry);
-    return {
-        mode: "development",
-        entry,
-        output: {
-            path: __dirname + "/dist",
-            filename: 'bundle.js'
-        },
-        resolve: {
-            alias: {
-                'vue$': 'vue/dist/vue.esm.js',
-                'd3-scale': 'd3-scale/dist/d3-scale.min.js'
-            },
-            extensions: ['*', '.js', '.vue', '.json']
-        },
-        module: {
-            rules: [{
-                test: /\.(vue)$/,
-                use: ['vue-loader']
-            }, {
-                test: /\.html$/,
-                use: ['html-loader']
-            }, {
-                test: /\.less$/,
-                use: ['style-loader', 'css-loader', 'less-loader']
-            }]
-        },
-        plugins: [
-            new VueLoaderPlugin()
-        ]
-    }
+  let entry = env.MAIN || './src/main/basic.js';
+  console.log(entry);
+  return {
+    mode: "development",
+    entry,
+    output: {
+      path: __dirname + "/dist",
+      filename: 'bundle.js'
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+        'd3-scale': 'd3-scale/dist/d3-scale.min.js'
+      },
+      extensions: ['*', '.js', '.vue', '.json']
+    },
+    module: {
+      rules: [{
+        test: /\.(vue)$/,
+        use: ['vue-loader']
+      }, {
+        test: /\.html$/,
+        use: ['html-loader']
+      }, {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      }]
+    },
+    plugins: [
+      new VueLoaderPlugin()
+    ]
+  }
 }
