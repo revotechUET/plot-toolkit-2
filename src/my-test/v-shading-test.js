c8fd35c31c52e335371db9562200cdbd55bc62b2
Sang Pham <psang242001@gmail.com>
2021-04-23 20:50:30 +0700

    fix v-shading

--- src/my-test/v-shading-test.js
+++ src/my-test/v-shading-test.js
@@ -16,12 +16,14 @@ new Vue({
                 fill-color="rgba(255, 0, 0, 0.3)" 
                 :real-min-x="realMinX" :real-max-x="realMaxX"
                 :real-min-y="realMinY" :real-max-y="realMaxY"
-                :real-right="6.2"
-                :real-left="realPath"
+                :real-right="realPath1"
+                :real-left="18"
                 line-color="#000000"
+                min-color="#F0F000"
+                max-color="#0000FF"
                 :shading-direction="shadingDirection">
             </v-shading>
-            <v-path :real-path="realPathObjFormat" symbol-color="0xFF0000">
+            <v-path :real-path="realPathObjFormat1" symbol-color="0xFF0000">
             </v-path>
         </v-scene>
     </fragment>`,
@@ -29,26 +31,54 @@ new Vue({
         return {
             viewWidth: 200,
             viewHeight: 500,
-            realMinX: 6,
+            realMinX: 14,
             realMaxX: 30,
             realMinY: 325,
             realMaxY: 3500,
             shadingDirection: "left",
-            realPath: [
-                20.32, 500, 25.32, 1200, 15.5, 2000, 20.32, 2500, 15.32, 3200
+            realPath1: [
+                { x: 18, y: 400 },
+                { x: 20.32, y: 650 },
+                { x: 15.5, y: 850 },
+                { x: 22, y: 1000 },
+                { x: 25.32, y: 1200 },
+                { x: 20, y: 1600 },
+                { x: 15.5, y: 2000 },
+                { x: 20.32, y: 2500 },
+                { x: 18.32, y: 3200 }
+            ],
+            realPath2: [
+                { x: 15, y: 400 },
+                { x: 23.32, y: 650 },
+                { x: 18.5, y: 850 },
+                { x: 25, y: 1000 },
+                { x: 28.32, y: 1200 },
+                { x: 23, y: 1600 },
+                { x: 18.5, y: 2000 },
+                { x: 23.32, y: 2500 },
+                { x: 15.32, y: 3200 }
             ],
         }
     },
     computed: {
-        realPathObjFormat: function () {
+        realPathObjFormat1: function () {
             let res = [];
-            for (let i = 0; i < this.realPath.length; i += 2) {
+            for (let i = 0; i < this.realPath1.length; i += 2) {
                 res.push({
-                    x: this.realPath[i],
-                    y: this.realPath[i + 1]
+                    x: this.realPath1[i],
+                    y: this.realPath1[i + 1]
                 })
             }
-            console.log(res);
+            return res;
+        },
+        realPathObjFormat2: function () {
+            let res = [];
+            for (let i = 0; i < this.realPath2.length; i += 2) {
+                res.push({
+                    x: this.realPath2[i],
+                    y: this.realPath2[i + 1]
+                });
+            }
             return res;
         }
     },
--- src/v-shading/index.js
+++ src/v-shading/index.js
@@ -1,73 +1,163 @@
 import VShape from "../v-shape";
-import { getColor, DefaultValues, getPosX, getPosY } from "../utils";
+import { processColorStr, getTransparency, DefaultValues, getPosX, getPosY } from "../utils";
+import { scaleLinear } from "d3-scale";
 
 async function draw(obj) {
     obj.clear();
-    let lw = this.lineWidth || 1;
-    // let lt = this.lineTransparency || 1.0;
-    // this.shadingSide = this.shadingDirection || 'left';
-    // this.path = this.realPath;
-    // obj.lineStyle(lw, this.cLineColor.color, this.cLineColor.transparency, 0);
-    obj.beginFill(
-        this.cFillColor.color,
-        this.cFillColor.transparency
-    );
+    console.log(this.cPolygonList);
 
-    let minX = Math.min(...this.cShadingPath.filter((item, index) => index % 2 === 0));
-    console.log('minX', minX);
+    let myFillColor;
+    let transformColor = scaleLinear().domain([this.realMinX, this.realMaxX]).range([this.minColor, this.maxColor]);
+    this.cPolygonList.forEach((polygon, idx) => {
+        let posXFillColor;
+        if (polygon.length === 4) { // this polygon is quadrilateral
+            posXFillColor = polygon[0]["x"] > polygon[1]["x"] ? polygon[0]["x"] : polygon[1]["x"];
+        } else { // this polygon is not quadriateral => it must be triangle
+            posXFillColor = Math.min(polygon[0]["x"], polygon[1]['x']);
+            if (idx >= 1 && this.cPolygonList[idx - 1].length === 3 && polygon[0]["x"] < polygon[1]["x"]) {
+                posXFillColor = polygon[2]["x"];
+            } else {
+                posXFillColor = Math.min(polygon[0]["x"], polygon[1]['x']);
+            }
+        }
+        console.log("pos x fill color", posXFillColor);
+        myFillColor = processColorStr(transformColor(posXFillColor), DefaultValues.fillColor)
+        obj.beginFill(
+            myFillColor.color,
+            myFillColor.transparency
+        );
+        // obj.drawPolygon(polygon);
+        this.myDrawPolygon(obj, polygon);
+        obj.endFill();
+    });
 
-    obj.drawPolygon(this.cShadingPath);
-
-    if (!Array.isArray(this.realLeft)) {
-        obj.moveTo(this._getX(this.realLeft), this.cShadingPath[1]);
-        obj.lineStyle(lw, this.cLineColor.color, this.cLineColor.transparency, 0);
-        obj.lineTo(this._getX(this.realLeft), this.cShadingPath[this.cShadingPath.length - 1]);
-    }
-
-    if (!Array.isArray(this.realRight)) {
-        obj.moveTo(this._getX(this.realRight), this.cShadingPath[1]);
-        obj.lineStyle(lw, this.cLineColor.color, this.cLineColor.transparency, 0);
-        obj.lineTo(this._getX(this.realRight), this.cShadingPath[this.cShadingPath.length - 1]);
-    }
-
-    obj.endFill();
     obj.x = getPosX(this.coordinate, this.posX);
     obj.y = getPosY(this.coordinate, this.posY);
     obj.rotation = this.rotation || 0;
+}
+
+function getLinearLine(point1, point2, num = 0) {
+    let res = {};
+    let a = (point1["y"] - point2['y']) / (point1["x"] - point2["x"]);
+    let b = point1["y"] - a * point1["x"];
+    num ? res[`a${num}`] = a : res['a'] = a;
+    num ? res[`b${num}`] = b : res['b'] = b;
+    return res;
+}
+
+function generatePolygons(path, bothIsArr) {
+    console.log("path", path);
+    let polygonArr = [];
+    if (!bothIsArr) {
+        let x = path[0]["x"];
+        let polygon;
+        let i = 1;
+        while (i < path.length - 2) {
+            // polygon = [x, path[i][1], ...path.slice(i, i + 4), x, path[i + 3]];
+            polygon = [{ x, y: path[i]["y"] }, ...path.slice(i, i + 2), { x, y: path[i + 1]["y"] }];
+            if ((polygon[1]["x"] >= x && polygon[2]["x"] < x) || (polygon[1]["x"] <= x && polygon[2]["x"] > x)) {
+                let { a, b } = getLinearLine(polygon[1], polygon[2]);
+                polygonArr.push([...polygon.slice(0, 2), { x, y: a * x + b }]);
+                polygonArr.push([...polygon.slice(2, 4), { x, y: a * x + b }]);
+            } else {
+                polygonArr.push(polygon);
+            }
+            i += 1;
+        }
+    } else {
+        let arr = [];
+        for (let i = 0; i < path.length; i += 2) {
+            arr.push(path.slice(i, i + 2));
+        }
+        for (let i = 0; i < arr.length - 1; i++) {
+            console.log(i, i + 1, arr[i], arr[i + 1]);
+            let mx = Math.max(arr[i + 1][0]["x"], arr[i + 1][1]["x"]);
+            let indexMx;
+            arr[i + 1].forEach((item, idx) => {
+                if (item.x === mx) {
+                    indexMx = idx;
+                }
+            });
+            console.log("index max", indexMx);
+            let x, y;
+            let secondPath = [];
+            if (arr[i][0]["x"] < arr[i][1]["x"]) {
+                if (indexMx === 0) {
+                    let { a1, b1 } = getLinearLine(arr[i][1], arr[i + 1][1], 1);
+                    let { a2, b2 } = getLinearLine(arr[i][0], arr[i + 1][0], 2);
+                    x = (b2 - b1) / (a1 - a2);
+                    y = a1 * x + b1;
+                } else {
+                    secondPath.push(arr[i + 1][1], arr[i + 1][0]);
+                }
+            } else {
+                if (indexMx === 1) {
+                    let { a1, b1 } = getLinearLine(arr[i][1], arr[i + 1][1], 1);
+                    let { a2, b2 } = getLinearLine(arr[i][0], arr[i + 1][0], 2);
+                    x = (b2 - b1) / (a1 - a2);
+                    y = a1 * x + b1;
+                } else {
+                    secondPath.push(arr[i + 1][1], arr[i + 1][0]);
+                }
+            }
+            if (x && y) {
+                polygonArr.push([...arr[i], { x, y }]);
+                polygonArr.push([...arr[i + 1], { x, y }]);
+            } else {
+                polygonArr.push([...arr[i], ...secondPath]);
+            }
+        }
+    }
+    return polygonArr;
 }
 
 let component = {
     props: [
         "realLeft",
         "realRight",
+        "minColor",
+        "maxColor"
     ],
     computed: {
-        cShadingPath: function () {
-            let path, begin, end;
-            if (Array.isArray(this.realLeft)) {
-                path = this.realLeft.map((item, idx) => idx % 2 ? this._getY(item) : this._getX(item));
-                if (!Array.isArray(this.realRight)) {
-                    let pixelRight = this._getX(this.realRight);
-                    console.log("pixcel", pixelRight);
-                    begin = [pixelRight || this.viewPosX, path[1]];
-                    end = [pixelRight || this.viewPosX, path[path.length - 1]];
+        cPolygonList: function () {
+            let begin, end, path = [];
+            let bothIsArr = Array.isArray(this.realLeft) && Array.isArray(this.realRight);
+            if (bothIsArr) {
+                let pathLeft = this.realLeft;
+                let pathRight = this.realRight;
+                let i = 0, j = 0;
+                while (i < pathLeft.length && j < pathRight.length) {
+                    path.push({ x: pathLeft[i]["x"], y: pathLeft[i]["y"] });
+                    path.push({ x: pathRight[j]["x"], y: pathRight[j]["y"] });
+                    i += 1;
+                    j += 1;
                 }
-            } else {
-                path = this.realRight.map((item, idx) => idx % 2 ? this._getY(item) : this._getX(item));
-                if (!Array.isArray(this.realLeft)) {
-                    let pixelLeft = this._getX(this.realLeft);
-                    console.log("pixel", pixelLeft);
-                    begin = [pixelLeft || this.viewPosX, path[1]];
-                    end = [pixelLeft || this.viewPosX, path[path.length - 1]];
-                }
+
             }
-            path = [...begin, ...path, ...end];
-            console.log(path);
-            return path;
-        }
+            else {
+                if (Array.isArray(this.realLeft) && !Array.isArray(this.realRight)) {
+                    path = this.realLeft;
+                    begin = { x: this.realRight, y: path[0]["y"] };
+                    end = { x: this.realRight, y: path[path.length - 1]["y"] };
+                } else if (!Array.isArray(this.realLeft) && Array.isArray(this.realRight)) {
+                    path = this.realRight;
+                    begin = { x: this.realLeft, y: path[0]["y"] };
+                    end = { x: this.realLeft, y: path[path.length - 1]["y"] };
+                }
+                path = [begin, ...path, end];
+            }
+            return generatePolygons(path, bothIsArr);
+        },
     },
     methods: {
         draw,
+        myDrawPolygon(obj, polygon) {
+            let res = [];
+            polygon = polygon.forEach(item => {
+                res.push(this._getX(item.x), this._getY(item.y));
+            });
+            obj.drawPolygon(res);
+        }
     },
 };
 
