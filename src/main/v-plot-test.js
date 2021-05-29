0b7a1e0a3fe22fd4136be063119a56ba506874fb
Tuanndq <quangtuanxyz@gmail.com>
2021-05-29 16:16:01 +0700

    update store

--- src/main/v-plot-test.js
+++ src/main/v-plot-test.js
@@ -11,7 +11,7 @@ const store = new Vuex.Store( options );
 let app = new Vue({
     el: "#vue-app",
     template: `<div>
-        <v-plot :id-project="1" :id-plot="2" />
+        <v-plot :id-project="1" :id-plot="3" />
     </div>
     `,
     data: function() {
--- src/stores/logplot-store.js
+++ src/stores/logplot-store.js
@@ -3,25 +3,46 @@ import axios from 'axios';
 export default {
     namespaced: true,
     state: () => ({
-        plot: null
+        plot: null,
+        zone_tracks: null,
+        tracks: null,
     }),
     mutations: {
         getData(state, plot) {
             console.log('Update plot');
             state.plot = plot;
+        },
+        getZoneTracks(state, zoneTrack) {
+            state.zone_tracks = zoneTrack;
+        },
+        getTracks(state, track) {
+            state.tracks = track;
         }
     },
     actions: {
-        getData({ state, commit }, { idProject, idPlot }) {
+        async getData({ state, commit }, { idProject, idPlot }) {
             console.log('Get data');
-            axios.post('http://112.137.129.214:35280/quangtuan/project/plot/info', {
+            const data = await axios.post('http://112.137.129.214:35280/quangtuan/project/plot/info', {
                 idProject: idProject,
                 idPlot: idPlot
-            })
-                .then(response => {
-                    console.log(response);
-                    commit('getData', response.data);
-                })
+            });
+            let { top, bottom } = JSON.parse(data.data.currentState);
+            console.log(top, bottom)
+            commit('getData', data.data);
+
+            const zone_tracks = await axios.post('http://112.137.129.214:35280/quangtuan/project/plot/zone_track/info', {
+                idProject: idProject,
+                idPlot: idPlot
+            });
+            commit('getZoneTracks', zone_tracks.data);
+
+            const tracks = await axios.post('http://112.137.129.214:35280/quangtuan/project/plot/track/info', {
+                idProject: idProject,
+                idPlot: idPlot,
+                top: parseInt(top),
+                bottom: parseInt(bottom)
+            });
+            commit('getTracks', tracks.data);
         }
     }
 }
